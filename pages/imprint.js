9d9edc4dca9b0311befade37e4092b34e039d261
David Ruisinger <david.ruisinger@gmail.com>
2022-02-18 11:32:59 +0100

    feat: added privacy & imprint pages

---
+++ pages/imprint.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ImprintPage() {
+  return <h1>This will become the imprint page</h1>
+}
---
+++ pages/privacy.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ImprintPage() {
+  return <h1>This will become the privacy page</h1>
+}
