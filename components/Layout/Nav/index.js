5cd3266bfbdac2dce5acf16a9fefaf2a8aa76149
Timo Müller <timo.h.mueller@gmail.com>
2022-02-28 16:16:36 +0200

    styling

--- components/Layout/Nav/index.js
+++ components/Layout/Nav/index.js
@@ -1,14 +1,28 @@
 require('./styles.less')
+import Link from 'next/link'
 import React from 'react'
+
+import { IntlSelector } from '../../IntlSelector'
+
+const LINKS = [
+  { title: 'Datenschutz', url: '/privacy' },
+  { title: 'Impressum', url: '/imprint' },
+]
 
 export const Nav = (props) => {
   return (
     <nav className={`${props.className} nav`}>
       <ul>
-        <li>Item</li>
-        <li>Item</li>
-        <li>Item</li>
-        <li>Item</li>
+        {LINKS.map((link, i) => {
+          return (
+            <li key={`link-${i}`}>
+              <Link href={link.url}>{link.title}</Link>
+            </li>
+          )
+        })}
+        <li>
+          <IntlSelector />
+        </li>
       </ul>
     </nav>
   )
