410ae3972ef0d98d9d5d7fbe9c9bea1e2a17ec0c
Timo Müller <timo.h.mueller@gmail.com>
2022-03-17 11:08:38 +0200

    add footer

--- components/Layout/DefaultLayout/index.js
+++ components/Layout/DefaultLayout/index.js
@@ -6,6 +6,7 @@ import { useNavs } from '../../../hooks/useTranslation'
 import { DefaultHero } from '../../Elements/DefaultHero'
 import Template from '../'
 import { DefaultNav } from '../DefaultNav'
+import { Footer } from '../Footer'
 
 export const DefaultLayout = ({ children, style, subtitle, title }) => {
   const nav = useNavs('overview')
@@ -14,8 +15,8 @@ export const DefaultLayout = ({ children, style, subtitle, title }) => {
     <Template className="default-layout">
       <DefaultNav data={nav} style={style} />
       <DefaultHero style={style} subtitle={subtitle} title={title} />
-
       <main className="container">{children}</main>
+      <Footer />
     </Template>
   )
 }
