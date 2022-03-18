ec5cd3cb05761c34a2faf57bfe58d4ffe9f33871
David Ruisinger <david.ruisinger@gmail.com>
2022-03-18 15:47:52 +0100

    fix: missing option wrapper

--- components/Elements/SelectFilter/Select/index.js
+++ components/Elements/SelectFilter/Select/index.js
@@ -13,18 +13,16 @@ export const Select = ({ items = [], mode, ...props }) => {
       getPopupContainer={() => document.getElementById('scroll-container')}
     >
       {items.map((option) => (
-        <AntdSelect key={option.value} value={option.value}>
-          <AntdSelect.Option>
-            <span className="label-wrapper">
-              {option.iconUrl && (
-                <span className="icon">
-                  <Image height={25} src={option.iconUrl} width={25} />
-                </span>
-              )}
-              {option.label}
-            </span>
-          </AntdSelect.Option>
-        </AntdSelect>
+        <AntdSelect.Option key={option.value} value={option.value}>
+          <span className="label-wrapper">
+            {option.iconUrl && (
+              <span className="icon">
+                <Image height={25} src={option.iconUrl} width={25} />
+              </span>
+            )}
+            {option.label}
+          </span>
+        </AntdSelect.Option>
       ))}
     </AntdSelect>
   )
