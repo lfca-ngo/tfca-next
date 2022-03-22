1e18f1db29f8fa115d21fc720f82b4aab8f95e7d
Timo Müller <timo.h.mueller@gmail.com>
2022-03-22 18:27:21 +0200

    add beta flag

--- components/Layout/SplitLayout/index.js
+++ components/Layout/SplitLayout/index.js
@@ -26,9 +26,10 @@ const SplitLayout = ({ children, company, layout, nav }) => {
       <Header actions={nav} collapsed={collapsed} setCollapsed={setCollapsed} />
       <div className={classNames({ collapsed })} id="scroll-container">
         <main>
-          <TopBar />
           <Nav className="hidden md" company={company} />
+
           <Hero onClick={() => scrollToId(nav[0]?.id)} />
+          <TopBar />
           {children}
           <Footer />
         </main>
--- components/Layout/TopBar/index.js
+++ components/Layout/TopBar/index.js
@@ -5,13 +5,18 @@ import { Button } from 'antd'
 import classNames from 'classnames'
 import React, { useState } from 'react'
 
+import { useTopbar } from '../../../hooks'
+
 export const TopBar = () => {
+  const topbarContent = useTopbar()
   const [hidden, setHidden] = useState(false)
+
+  if (!topbarContent.active) return null
   return (
     <div className={classNames('top-bar', { hidden })}>
-      <div className="text">{`👋 You are seeing a BETA version`}</div>
+      <div className="text">{topbarContent.text}</div>
       <Button className="no-padding" type="link">
-        Under construction
+        {topbarContent?.link?.title}
       </Button>
       <Button
         className="no-padding close-btn"
