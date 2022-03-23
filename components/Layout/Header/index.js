e4bb91bff8c57a6d48fbd7766966a48ab5b3aec0
Timo Müller <timo.h.mueller@gmail.com>
2022-03-23 13:09:16 +0200

    add q and a to drawer (#44)
    * move animations to const
    
    * add q and a to drawer

--- components/Layout/Header/index.js
+++ components/Layout/Header/index.js
@@ -10,23 +10,30 @@ import React from 'react'
 import { useActiveAction, useDarkMode, useIsMobile } from '../../../hooks'
 import { getLogoSrc, scrollToId } from '../../../utils'
 import { Hamburger } from '../../Elements/Hamburger'
+import { QuestionAnswer } from '../../Elements/QuestionAnswer'
 
 const SCROLL_RANGE = [0, 400]
 const SCROLL_RANGE_SHORT = [0, 60]
+const CONTENT_WIDTH_RANGE = [0, 375]
+const OPACITY_RANGE = [0, 1]
+const HEADER_WIDTH_RANGE = ['100%', '16%']
+const LOGO_PADDING_RANGE = ['20px', '0px']
+const BOX_SHADOW_RANGE = [
+  '0px 10px 10px rgba(0, 0, 0, 0.0)',
+  '0px 10px 10px rgba(0, 0, 0, 0.05)',
+]
 
 export const Header = ({ actions, collapsed, setCollapsed }) => {
   const [isDarkMode] = useDarkMode()
   const isMobile = useIsMobile()
-  // animations for mobile, the event does not fire on desktop
   const { scrollY } = useViewportScroll()
-  const contentWidth = useTransform(scrollY, SCROLL_RANGE, [0, 375])
-  const opacity = useTransform(scrollY, SCROLL_RANGE_SHORT, [1, 0])
-  const headerWidth = useTransform(scrollY, SCROLL_RANGE, ['100%', '16%'])
-  const logoPadding = useTransform(scrollY, SCROLL_RANGE, ['20px', '0px'])
-  const boxShadow = useTransform(scrollY, SCROLL_RANGE, [
-    '0px 10px 10px rgba(0, 0, 0, 0.0)',
-    '0px 10px 10px rgba(0, 0, 0, 0.05)',
-  ])
+
+  // animations for mobile, the event does not fire on desktop
+  const contentWidth = useTransform(scrollY, SCROLL_RANGE, CONTENT_WIDTH_RANGE)
+  const opacity = useTransform(scrollY, SCROLL_RANGE_SHORT, OPACITY_RANGE)
+  const headerWidth = useTransform(scrollY, SCROLL_RANGE, HEADER_WIDTH_RANGE)
+  const logoPadding = useTransform(scrollY, SCROLL_RANGE, LOGO_PADDING_RANGE)
+  const boxShadow = useTransform(scrollY, SCROLL_RANGE, BOX_SHADOW_RANGE)
 
   const logoStyles = isMobile ? { opacity, padding: logoPadding } : {}
   const logoSrc = getLogoSrc(isDarkMode)
@@ -43,7 +50,7 @@ export const Header = ({ actions, collapsed, setCollapsed }) => {
           <Image height={48} src={logoSrc} width={48} />
         </motion.div>
 
-        <Hamburger />
+        <Hamburger content={<QuestionAnswer />} />
       </motion.div>
 
       <motion.div
