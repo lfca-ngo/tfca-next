7d5e32e1f8c6e8796a2a457c45fe861bc301f52f
David Ruisinger <david.ruisinger@gmail.com>
2022-03-28 12:27:35 +0200

    feat: store energy form values

--- components/ActionModules/SwitchEnergy/index.js
+++ components/ActionModules/SwitchEnergy/index.js
@@ -46,7 +46,11 @@ export const SwitchEnergy = ({ module }) => {
         {stepsKeys.map((key, i) => {
           const Page = steps.get(key)
           const nextKey = i <= stepsKeys.length ? stepsKeys[i + 1] : null
-          const prevKey = i > 0 ? stepsKeys[i - 1] : null
+          let prevKey = i > 0 ? stepsKeys[i - 1] : null
+
+          if (key === 'success') {
+            if (!store.form) prevKey = 'check'
+          }
 
           return (
             <TabPane key={key} tab={key}>
