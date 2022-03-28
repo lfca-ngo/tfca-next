741dd59f22ff7ae7033bca74053ffe3354e19c35
Timo Müller <timo.h.mueller@gmail.com>
2022-03-28 18:28:33 +0300

    move hero title to cms

--- utils/names-array-to-string.js
+++ utils/names-array-to-string.js
@@ -1,9 +1,5 @@
-export function namesArrayToString(names = ['']) {
+export function namesArrayToString(names = [''], fallbackString) {
   if (names.length === 1) {
     return names[0]
-  } else {
-    return `${names.slice(0, names.length - 1).join(', ')} and ${
-      names[names.length - 1]
-    }`
-  }
+  } else fallbackString
 }
