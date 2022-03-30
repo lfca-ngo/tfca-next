1610524c995542a1885379c0003e6bbcbf447230
Timo Müller <timo.h.mueller@gmail.com>
2022-03-30 14:51:12 +0300

    add basic detail view for tool

---
+++ components/Elements/DetailViews/ToolDetails.js
@@ -0,0 +1,76 @@
+require('./toolDetails.less')
+
+import {
+  CheckOutlined,
+  PlusCircleOutlined,
+  ShareAltOutlined,
+} from '@ant-design/icons'
+import { Button, List, Space } from 'antd'
+import Image from 'next/image'
+import React from 'react'
+
+import { Text, text } from '../../../utils/Text'
+import CallToAction from '../CallToAction'
+
+export const ToolDetails = ({ item }) => {
+  return (
+    <div className="detail-view tool">
+      <div className="header">
+        <div className="text">
+          <div className="title">{item.name}</div>
+        </div>
+
+        <div className="info-wrapper">
+          <div className="logo-wrapper">
+            <Image layout="fill" objectFit="contain" src={item.logo?.url} />
+          </div>
+          <div className="actions">
+            <Space style={{ width: '100%' }}>
+              <Button
+                block
+                icon={<ShareAltOutlined />}
+                shape="round"
+                type="primary"
+              />
+              <Button block shape="round" type="primary">
+                Call to Action
+              </Button>
+            </Space>
+          </div>
+        </div>
+      </div>
+
+      <div className="activities">
+        <div className="section-title title">
+          <PlusCircleOutlined />
+          Activities
+        </div>
+
+        <List
+          className="simple-list"
+          dataSource={item?.benefitsCollection?.items}
+          renderItem={(item) => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={<CheckOutlined />}
+                description={text(item.value)}
+              />
+            </List.Item>
+          )}
+        />
+      </div>
+
+      <div className="description">
+        <div className="section-title title">
+          <PlusCircleOutlined />
+          About
+        </div>
+        <Text block={item?.description} />
+      </div>
+
+      {item?.actionsCollection?.items?.map((action, i) => (
+        <CallToAction key={`action-${i}`} showLeaveModal {...action} />
+      ))}
+    </div>
+  )
+}
