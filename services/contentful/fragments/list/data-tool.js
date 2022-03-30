eb75fd543b87e4cede5a1d7a24ef88347d471a98
Timo Müller <timo.h.mueller@gmail.com>
2022-03-30 14:40:14 +0300

    basic card for data tools

--- services/contentful/fragments/list/data-tool.js
+++ services/contentful/fragments/list/data-tool.js
@@ -3,6 +3,21 @@ import { gql } from 'graphql-request'
 export const DataToolFragment = gql`
   fragment DataToolFragment on DataTool {
     name
+    title
+    tagline
+    description {
+      json
+    }
+    benefitsCollection(limit: 5) {
+      items {
+        ...BlockFragment
+      }
+    }
+    actionsCollection {
+      items {
+        ...CallToActionFragment
+      }
+    }
     logo {
       url
     }
