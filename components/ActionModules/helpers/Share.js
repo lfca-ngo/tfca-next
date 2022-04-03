0b9277ba37305148695c01d7fce835328e72f63d
Timo Müller <timo.h.mueller@gmail.com>
2022-04-03 11:59:01 +0300

    add share buttons

--- components/ActionModules/helpers/Share.js
+++ components/ActionModules/helpers/Share.js
@@ -1,10 +1,27 @@
 require('./share.less')
 
-import { Button, Input, message, Tabs } from 'antd'
+import Icon from '@ant-design/icons'
+import {
+  FacebookOutlined,
+  LinkedinOutlined,
+  MailOutlined,
+  TwitterOutlined,
+  WhatsAppOutlined,
+} from '@ant-design/icons'
+import { Button, Input, message, Space, Tabs } from 'antd'
 import Image from 'next/image'
 import React from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  LinkedinShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from 'react-share'
 
+import TelegramIcon from '../../../assets/icons/telegram.svg'
 import { namesArrayToString } from '../../../utils'
 import { CopyTextArea } from '../../Elements/CopyTextArea'
 import { SuperText } from '../../Elements/SuperText'
@@ -35,24 +52,11 @@ export const Share = ({ imageInviteText, invites }) => {
       <Tabs defaultActiveKey="0">
         {invites.map(({ names, ogImageUrl, shortLink }, i) => {
           const tabName = names.length === 1 ? names[0] : 'All'
-
+          const inviteText = createInviteText(names)
+          const inviteTitle = `It's time to take climate action`
           return (
             <TabPane key={`${i}`} tab={tabName}>
-              <CopyTextArea
-                rows={4}
-                text={createInviteText(names)}
-                textSize="large"
-              />
-
-              <Image
-                alt={tabName}
-                height={630}
-                layout="intrinsic"
-                src={ogImageUrl}
-                style={{ borderRadius: '12px', margin: '20px 0' }}
-                unoptimized={true}
-                width={1200}
-              />
+              <CopyTextArea rows={4} text={inviteText} textSize="large" />
 
               <div className="share-image-preview">
                 <Image
@@ -61,6 +65,7 @@ export const Share = ({ imageInviteText, invites }) => {
                   layout="intrinsic"
                   src={ogImageUrl}
                   style={{ borderRadius: '12px', margin: '20px 0' }}
+                  unoptimized={true}
                   width={1200}
                 />
               </div>
@@ -83,6 +88,65 @@ export const Share = ({ imageInviteText, invites }) => {
                   </Button>
                 </CopyToClipboard>
               </Input.Group>
+
+              <Space
+                className="share-buttons"
+                direction="vertical"
+                style={{ width: '100%' }}
+              >
+                <WhatsappShareButton
+                  className="ant-btn ant-btn-primary share-btn"
+                  resetButtonStyle={false}
+                  title={inviteText}
+                  url={shortLink}
+                >
+                  <WhatsAppOutlined />
+                  Share on WhatsApp
+                </WhatsappShareButton>
+                <FacebookShareButton
+                  className="ant-btn ant-btn-primary share-btn"
+                  quote={inviteText}
+                  resetButtonStyle={false}
+                  url={shortLink}
+                >
+                  <FacebookOutlined />
+                  Share on Facebook
+                </FacebookShareButton>
+                <TelegramShareButton
+                  className="ant-btn ant-btn-primary share-btn"
+                  resetButtonStyle={false}
+                  title={inviteText}
+                  url={shortLink}
+                >
+                  <Icon component={TelegramIcon} />
+                  Share on Telegram
+                </TelegramShareButton>
+                <TwitterShareButton
+                  className="ant-btn ant-btn-primary share-btn"
+                  resetButtonStyle={false}
+                  url={shortLink}
+                >
+                  <TwitterOutlined /> Share on Twitter
+                </TwitterShareButton>
+                <LinkedinShareButton
+                  className="ant-btn ant-btn-primary share-btn"
+                  resetButtonStyle={false}
+                  summary={inviteText}
+                  title={inviteTitle}
+                  url={shortLink}
+                >
+                  <LinkedinOutlined /> Share on Linkedin
+                </LinkedinShareButton>
+                <EmailShareButton
+                  body={inviteText}
+                  className="ant-btn ant-btn-primary share-btn"
+                  resetButtonStyle={false}
+                  subject={inviteTitle}
+                  url={shortLink}
+                >
+                  <MailOutlined /> Share via Email
+                </EmailShareButton>
+              </Space>
             </TabPane>
           )
         })}
