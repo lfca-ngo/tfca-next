082acd142286f9733e018420100e3fb67ca8ebed
David Ruisinger <david.ruisinger@gmail.com>
2022-04-08 13:48:42 +0200

    fix: doubplicated tracking calls when locale changes

--- hooks/analytics.js
+++ hooks/analytics.js
@@ -8,19 +8,17 @@ export const useAnalytics = () => {
   const { locale, query } = useRouter()
   const customization = useCustomization()
 
-  const trackEvent = useCallback(
-    ({ collection, name, values }) => {
-      trackRaw({
-        action_collection_slug: query.actionCollectionSlug,
-        collection,
-        inviting_uid: customization?.uid,
-        locale,
-        name,
-        values,
-      })
-    },
-    [customization, locale, query]
-  )
+  const trackEvent = useCallback(({ collection, name, values }) => {
+    trackRaw({
+      action_collection_slug: query.actionCollectionSlug,
+      collection,
+      inviting_uid: customization?.uid,
+      locale,
+      name,
+      values,
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return {
     trackEvent,
