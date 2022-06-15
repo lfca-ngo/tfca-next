475eefaf60f77bb7f5af91ce5331c2d857064612
David Ruisinger <david.ruisinger@gmail.com>
2022-06-15 18:02:30 +0200

    chore: remove unused prop

--- services/lfca/fragments/qualified-company-item.js
+++ services/lfca/fragments/qualified-company-item.js
@@ -2,7 +2,6 @@ import { gql } from 'graphql-request'
 
 export const QualifiedCompanyFragment = gql`
   fragment QualifiedCompanyFragment on Company {
-    campaignGoalSetting
     campaignParticipationPackages
     campaignGoals
     campaignFiles {
