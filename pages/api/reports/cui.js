962028307479ece59b596f3be6a94071fff9b763
David Ruisinger <david.ruisinger@gmail.com>
2022-08-15 16:22:08 +0200

    feat: updated CUI report format (#114)

--- pages/api/reports/cui.js
+++ pages/api/reports/cui.js
@@ -3,6 +3,8 @@ import { parse } from 'json2csv'
 import stream from 'stream'
 import { promisify } from 'util'
 
+import { getDateTimeFileSuffix } from '../../../utils-server-only'
+
 const pipeline = promisify(stream.pipeline)
 
 const OPTIONS_BY_TYPE = {
@@ -42,16 +44,6 @@ const OPTIONS_BY_TYPE = {
     query:
       "WITH completions AS ( SELECT toDate( toStartOfDay(toDateTime(timestamp, 'Europe/Berlin')) ) AS date, JSONExtractString(json, 'Event') as event_name, JSONExtractString(json, 'action_id') as action_id, CASE JSONExtractString(json, 'action_id') WHEN 'switch_energy' THEN 1648.7 ELSE 0 END as impact, CASE JSONExtractString(json, 'action_collection_slug') WHEN 'usa' THEN 'US' WHEN 'tur' THEN 'TR' WHEN 'deu' THEN 'DE' WHEN 'fra' THEN 'FR' WHEN 'esp' THEN 'ES' WHEN 'gbr' THEN 'UK' WHEN 'int' THEN '' ELSE '??' END as region, JSONExtractBool(json, 'consent') as consent, JSONExtractString(json, 'User_ID') as uid FROM base WHERE event_name = 'action_completed' AND consent = 1 AND region <> '??' ORDER BY date ASC LIMIT 1 BY uid, action_id ) SELECT 'lfca' as partner_id, COUNT(DISTINCT uid) as unique_user_count, date as action_completed_date, region as Geography, floor(SUM(impact), 6) AS Total_carbon_Savings, COUNT(action_id) as Total_Number_of_completed_actions FROM completions WHERE date > subtractDays(yesterday(), 7) AND date < today() GROUP BY date, region ORDER BY date DESC",
   },
-}
-
-function getDateTimeFileSuffix() {
-  const date = new Date()
-
-  return `${date.getFullYear()}_${('0' + (date.getMonth() + 1)).slice(-2)}_${(
-    '0' + date.getDate()
-  ).slice(-2)}_${('0' + date.getHours()).slice(-2)}_${(
-    '0' + date.getMinutes()
-  ).slice(-2)}_${('0' + date.getSeconds()).slice(-2)}`
 }
 
 export default async function handler(req, res) {
--- pages/api/reports/send-cui-report.js
+++ pages/api/reports/send-cui-report.js
@@ -1,5 +1,35 @@
 import mandrill from '@mailchimp/mailchimp_transactional'
+import axios from 'axios'
 import { parse } from 'json2csv'
+
+import { getDateTimeFileSuffix } from '../../../utils-server-only'
+
+const REPORTS = [
+  {
+    fields: [
+      'partner_id',
+      'action_level_1',
+      'action_completed_count',
+      'action_completed_savings',
+      'action_completed_date',
+      'action_country',
+    ],
+    filename: 'cui_actions_lfca_in',
+    query:
+      "WITH completions AS ( SELECT formatDateTime(toDateTime(timestamp, 'Europe/Berlin'), '%Y-%m') AS date, JSONExtractString(json, 'Event') as event_name, JSONExtractString(json, 'action_id') as action_id, CASE JSONExtractString(json, 'action_id') WHEN 'switch_energy' THEN 1648.7 ELSE 0 END as impact, CASE JSONExtractString(json, 'action_collection_slug') WHEN 'usa' THEN 'US' WHEN 'tur' THEN 'TR' WHEN 'deu' THEN 'DE' WHEN 'fra' THEN 'FR' WHEN 'esp' THEN 'ES' WHEN 'gbr' THEN 'UK' WHEN 'int' THEN '' ELSE '??' END as region, JSONExtractBool(json, 'consent') as consent, JSONExtractString(json, 'User_ID') as uid FROM base WHERE event_name = 'action_completed' AND consent = 1 AND region <> '??' ) SELECT 'lfca' as partner_id, CASE action_id WHEN 'climate_activism' THEN 'Voice' WHEN 'ecosia' THEN 'Lifestyle' WHEN 'green_finances' THEN 'Lifestyle' WHEN 'politics' THEN 'Voice' WHEN 'share_deck' THEN 'Lifestyle' WHEN 'switch_energy' THEN 'Home' WHEN 'food' THEN 'Food' ELSE '' END as action_level_1, COUNT(action_id) as action_completed_count, floor(SUM(impact), 2) AS action_completed_savings, date as action_completed_date, region as action_country FROM completions WHERE action_completed_date = formatDateTime(subtractMonths(today(), 1), '%Y-%m') GROUP BY action_level_1, date, region ORDER BY action_completed_date DESC",
+  },
+  {
+    fields: [
+      'partner_id',
+      'New_active_user',
+      'action_completed_date',
+      'Country',
+    ],
+    filename: 'cui_uniqueusers_lfca_in',
+    query:
+      "WITH completions AS ( SELECT formatDateTime(toDateTime(timestamp, 'Europe/Berlin'), '%Y-%m') AS date, JSONExtractString(json, 'Event') as event_name, JSONExtractString(json, 'action_id') as action_id, CASE JSONExtractString(json, 'action_collection_slug') WHEN 'usa' THEN 'US' WHEN 'tur' THEN 'TR' WHEN 'deu' THEN 'DE' WHEN 'fra' THEN 'FR' WHEN 'esp' THEN 'ES' WHEN 'gbr' THEN 'UK' WHEN 'int' THEN '' ELSE '??' END as region, JSONExtractBool(json, 'consent') as consent, JSONExtractString(json, 'User_ID') as uid FROM base WHERE event_name = 'action_completed' AND consent = 1 AND region <> '??' ORDER BY date ASC LIMIT 1 BY uid )  SELECT 'lfca' as partner_id, COUNT(DISTINCT uid) as New_active_user, date as action_completed_date, region as Country FROM completions WHERE action_completed_date = formatDateTime(subtractMonths(today(), 1), '%Y-%m') GROUP BY date, region ORDER BY date DESC",
+  },
+]
 
 export default async function handler(req, res) {
   if (req.headers.authorization !== `Bearer ${process.env.API_ADMIN_TOKEN}`) {
@@ -15,56 +45,41 @@ export default async function handler(req, res) {
     return res.status(500).send({ message: 'No recipients found.' })
   }
 
-  const actionsCsv = parse(
-    [
-      {
-        action_id: 'politics',
-        partner_id: 'lfca',
-      },
-      {
-        action_id: 'ecosia',
-        partner_id: 'lfca',
-      },
-    ],
-    {
-      delimiter: '|',
-      quote: '',
-    }
-  )
+  const attachments = []
 
-  const usersCsv = parse(
-    [
-      {
-        partner_id: 'lfca',
-        unique_user_count: 3,
-      },
-      {
-        partner_id: 'lfca',
-        unique_user_count: 1,
-      },
-    ],
-    {
-      delimiter: '|',
-      quote: '',
+  // Fetch reports from GraphJSON
+  for (const report of REPORTS) {
+    const payload = {
+      api_key: process.env.NEXT_PUBLIC_GRAPH_JSON_API_KEY,
+      graph_type: 'Table',
+      sql_query: report.query,
     }
-  )
+
+    const response = await axios({
+      data: payload,
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      url: 'https://api.graphjson.com/api/visualize/data',
+    })
+
+    const csv = parse(response.data.result, {
+      delimiter: '|',
+      fields: report.fields,
+      quote: '',
+    })
+
+    attachments.push({
+      content: Buffer.from(csv, 'utf8').toString('base64'),
+      name: `${report.filename}_${getDateTimeFileSuffix()}.csv`,
+      type: 'text/csv',
+    })
+  }
 
   const mandrillClient = mandrill(process.env.MANDRILL_API_KEY || '')
 
   await mandrillClient.messages.send({
     message: {
-      attachments: [
-        {
-          content: Buffer.from(actionsCsv, 'utf8').toString('base64'),
-          name: 'actions.csv',
-          type: 'text/csv',
-        },
-        {
-          content: Buffer.from(usersCsv, 'utf8').toString('base64'),
-          name: 'users.csv',
-          type: 'text/csv',
-        },
-      ],
+      attachments,
       from_email: 'info@lfca.earth',
       from_name: 'LFCA',
       subject: 'Montly CUI report',
