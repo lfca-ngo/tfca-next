5a7c4fb12b2c74a09d7c74bc426f7eedaae9fac7
Timo Müller <timo.h.mueller@gmail.com>
2023-02-02 13:33:57 +0200

    error boundary

--- components/ErrorBoundary/index.js
+++ components/ErrorBoundary/index.js
@@ -28,7 +28,7 @@ export class ErrorBoundary extends React.Component {
   }
 
   render() {
-    if (!this.state.hasError) {
+    if (this.state.hasError) {
       return (
         <div className="error-boundary">
           <h2>Something went wrong...</h2>
