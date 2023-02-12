57aa25692cdc0c9c3b9aba9ca37738cad707854c
Timo Müller <timo.h.mueller@gmail.com>
2023-02-12 12:45:54 +0200

    adjust styls

--- components/DisclosureOverview/index.js
+++ components/DisclosureOverview/index.js
@@ -31,110 +31,118 @@ export const DisclosureOverview = ({ items }) => {
 
   return (
     <div className="disclosure-overview">
-      <div className="navigation">
-        <Row>
-          <Col md={12} xs={24}>
-            <h3>All Companies</h3>
-          </Col>
-          <Col md={6} style={{ paddingRight: '14px' }} xs={12}>
-            <Select placeholder="Filter" size="large" style={{ width: '100%' }}>
-              <Select.Option key="material">-</Select.Option>
-            </Select>
-          </Col>
-          <Col md={6} xs={12}>
-            <Search
-              onSearch={handleSearch}
-              placeholder="Search a company"
-              size="large"
-            />
-          </Col>
-        </Row>
-      </div>
+      <div className="container-max">
+        <div className="navigation">
+          <Row>
+            <Col md={12} xs={24}>
+              <h3>All Companies</h3>
+            </Col>
+            <Col md={6} style={{ paddingRight: '14px' }} xs={12}>
+              <Select
+                placeholder="Filter"
+                size="large"
+                style={{ width: '100%' }}
+              >
+                <Select.Option key="material">-</Select.Option>
+              </Select>
+            </Col>
+            <Col md={6} xs={12}>
+              <Search
+                onSearch={handleSearch}
+                placeholder="Search a company"
+                size="large"
+              />
+            </Col>
+          </Row>
+        </div>
 
-      <div className="overview-wrapper">
-        <List
-          bordered={false}
-          className="fill-space"
-          dataSource={searchResults.length > 0 ? searchResults : items}
-          grid={{
-            gutter: 16,
-            lg: 2,
-            md: 2,
-            sm: 2,
-            xl: 4,
-            xs: 1,
-            xxl: 4,
-          }}
-          pagination={{ pageSize: 40, showSizeChanger: false }}
-          renderItem={(item) => {
-            const actionsList = item.completedCompanyActions.map((action) => ({
-              value: action.title,
-            }))
+        <div className="overview-wrapper">
+          <List
+            bordered={false}
+            className="fill-space"
+            dataSource={searchResults.length > 0 ? searchResults : items}
+            grid={{
+              gutter: 16,
+              lg: 2,
+              md: 2,
+              sm: 2,
+              xl: 4,
+              xs: 1,
+              xxl: 4,
+            }}
+            pagination={{ pageSize: 40, showSizeChanger: false }}
+            renderItem={(item) => {
+              const actionsList = item.completedCompanyActions.map(
+                (action) => ({
+                  value: action.title,
+                })
+              )
 
-            // We only show the participation packages where the company checked at least one objective
-            const contributionList = Object.keys(
-              item.campaignParticipationPackages || {}
-            ).map((c) => ({ value: c }))
+              // We only show the participation packages where the company checked at least one objective
+              const contributionList = Object.keys(
+                item.campaignParticipationPackages || {}
+              ).map((c) => ({ value: c }))
 
-            return (
-              <List.Item className="contribution-item">
-                <Card
-                  className="company-card"
-                  hoverable
-                  title={
-                    <div className="logo-wrapper no-border">
-                      <Image
-                        height={60}
-                        layout="intrinsic"
-                        objectFit="contain"
-                        src={item?.logoUrl}
-                        width={120}
-                      />
-                    </div>
-                  }
-                >
-                  <Tabs defaultActiveKey="a" size="small">
-                    <TabPane key="a" tab={'Actions'}>
-                      <CheckList data={actionsList} limit={3} />
-                      {actionsList.length > 3 && (
-                        <div className="show-more">
-                          ...click view to show all
-                        </div>
-                      )}
-                    </TabPane>
-                    <TabPane key="b" tab={'Contribution'}>
-                      <CheckList data={contributionList} limit={3} />
-                      {contributionList.length > 3 && (
-                        <div className="show-more">
-                          ...click view to show all
-                        </div>
-                      )}
-                    </TabPane>
-                  </Tabs>
-
-                  <Button
-                    block
-                    onClick={() => handleSelect(item)}
-                    size="large"
-                    style={{ alignSelf: 'flex-end' }}
-                    type="primary"
+              return (
+                <List.Item className="contribution-item">
+                  <Card
+                    className="company-card"
+                    hoverable
+                    title={
+                      <div className="logo-wrapper no-border">
+                        <Image
+                          height={60}
+                          layout="intrinsic"
+                          objectFit="contain"
+                          src={item?.logoUrl}
+                          width={120}
+                        />
+                      </div>
+                    }
                   >
-                    View all
-                  </Button>
-                </Card>
-              </List.Item>
-            )
-          }}
-        />
-      </div>
+                    <Tabs defaultActiveKey="a" size="small">
+                      <TabPane key="a" tab={'Actions'}>
+                        <CheckList data={actionsList} limit={3} />
+                        {actionsList.length > 3 && (
+                          <div className="show-more">
+                            ...click view to show all
+                          </div>
+                        )}
+                      </TabPane>
+                      <TabPane key="b" tab={'Contribution'}>
+                        <CheckList data={contributionList} limit={3} />
+                        {contributionList.length > 3 && (
+                          <div className="show-more">
+                            ...click view to show all
+                          </div>
+                        )}
+                      </TabPane>
+                    </Tabs>
 
-      <Drawer
-        className="drawer-md"
-        onClose={() => setDrawerVisible(!drawerVisible)}
-        visible={drawerVisible}
-      >
-        <Disclosure data={activeItem} />
-      </Drawer>
+                    <Button
+                      block
+                      onClick={() => handleSelect(item)}
+                      size="large"
+                      style={{ alignSelf: 'flex-end' }}
+                      type="primary"
+                    >
+                      View all
+                    </Button>
+                  </Card>
+                </List.Item>
+              )
+            }}
+          />
+        </div>
+
+        <Drawer
+          className="drawer-md"
+          onClose={() => setDrawerVisible(!drawerVisible)}
+          visible={drawerVisible}
+        >
+          <Disclosure data={activeItem} />
+        </Drawer>
+      </div>
     </div>
   )
 }
