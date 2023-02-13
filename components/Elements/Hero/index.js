623ca01a9fb6fba74b0688741f3af9ca9a83f18c
Timo Müller <timo.h.mueller@gmail.com>
2023-02-13 17:06:37 +0200

    move teams hero to cms

--- components/Elements/Hero/index.js
+++ components/Elements/Hero/index.js
@@ -20,12 +20,15 @@ export const Hero = ({ onClick, openGraphInfo, team }) => {
 
   const customization = useCustomization()
 
+  const teamBlock = useContentBlocks('hero.team.title')
+  const teamSubtitle = useContentBlocks('hero.team.body')
+
   const defaultBlock = useContentBlocks('header.title')
   const customBlock = useContentBlocks('header.title.custom')
+  const pageSubtitle = useContentBlocks('header.body')
   const recipientsFallback = textBlockToString(
     useContentBlocks('header.title.recipients.fallback')
   )
-  const pageSubtitle = useContentBlocks('header.body')
 
   const onResizeFinish = () => {
     setFinalContainerHeight(textRef?.current?.scrollHeight)
@@ -80,29 +83,30 @@ export const Hero = ({ onClick, openGraphInfo, team }) => {
               style={{ fontSize }}
             >
               <span ref={textRef}>
-                {teamId ? (
-                  <>
-                    Take action with{' '}
-                    <span className="highlight">{teamCapitalized}</span>{' '}
-                  </>
-                ) : customization?.invitedUserName ? (
-                  textBlockToString(
-                    customBlock,
-                    {
-                      name: customization.invitedUserName || recipientsFallback,
-                    },
-                    {},
-                    true
-                  )
-                ) : (
-                  textBlockToString(defaultBlock, {}, true)
-                )}
+                {teamId
+                  ? textBlockToString(
+                      teamBlock,
+                      { team: teamCapitalized },
+                      {},
+                      true
+                    )
+                  : customization?.invitedUserName
+                  ? textBlockToString(
+                      customBlock,
+                      {
+                        name:
+                          customization.invitedUserName || recipientsFallback,
+                      },
+                      {},
+                      true
+                    )
+                  : textBlockToString(defaultBlock, {}, true)}
               </span>
             </div>
           </h1>
           <p>
             {teamId
-              ? 'Your team is participating in the TFCA Teams Challenge. Collect points by completing actions and inviting friends!'
+              ? textBlockToString(teamSubtitle)
               : textBlockToString(pageSubtitle)}
           </p>
 
