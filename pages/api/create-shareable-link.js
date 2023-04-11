e5760343b78c6005ce5c4f1c54568418891f8684
Timo Müller <timo.h.mueller@gmail.com>
2023-04-11 17:33:50 +0300

    make sure that slug is never empty

--- pages/api/create-shareable-link.js
+++ pages/api/create-shareable-link.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   }
 
   const {
-    actionCollectionSlug,
+    actionCollectionSlug = 'int',
     invites = [],
     locale,
     referredByTeamId = null,
